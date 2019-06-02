be86821ccb9b0e76b6beae04db9bf2bb6a68f84c
Amy Cen <amy.cen@blackrock.com>
2019-06-02 02:58:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,7 @@ def create_project_hash
   #project: kickstarter.css("li.project.grid_4")
   #title: kickstarter.css("h2.bbcard_name strong a").text
   #image: kickstarter.css("div.project-thumbnail a img").attribute("src").value
+  #description: kickstarter.css("bbcard_blurb").text
 end
 
 create_project_hash
