27a8e8d6f9c3355e13e7886e3fa420629d1e7707
Amy Cen <amy.cen@blackrock.com>
2019-06-02 02:55:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,8 +6,8 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  #kickstarter.css("li.project.grid_4")
-  kickstarter.css("h2.bbcard_name strong a").text
+  #project: kickstarter.css("li.project.grid_4")
+  #title: kickstarter.css("h2.bbcard_name strong a").text
 end
 
 create_project_hash
